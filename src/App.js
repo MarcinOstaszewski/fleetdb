0548916b6093e4340d267606ae00a15f6a04ad89
MarcinOstaszewski <marcinostaszewski@gmail.com>
2018-02-11 19:20:30 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -85,6 +85,13 @@ class App extends Component {
 	}
 
 	componentDidMount() {
+
+		auth.onAuthStateChanged((user) => {
+			if (user) {
+			this.setState({ user });
+			} 
+		});
+
 		const carsRef = firebase.database().ref('cars');
 		carsRef.on('value', (snapshot) => {
 			let cars = snapshot.val();
@@ -110,8 +117,6 @@ class App extends Component {
 			});
 		});
 
-
-
 	
 	// USED ONLY FOR CREATING CARS_DB ON FIREBASE
 	// 	fetch(`https://vehiclefakedb.firebaseio.com/masterSheet.json`).then( r =>   r.json() ).then( response => {		
@@ -136,85 +141,106 @@ class App extends Component {
 
 
 	render() {
+		// let userName = (
+		// 	<div>
+		// 		<div className='rightAlign'>Witaj, {this.state.user.displayName || this.state.user.email} </div>
+		// 		<div onClick={this.logout}>Log Out</div>                
+		// 	</div>);
+
 		return (
 			<div className='app'>
 				<header>
 					<div className='wrapper'>
-							<div className='title'><h1>Car Fleet DB Manager</h1></div>
-							{this.state.user ?
-								<button onClick={this.logout}>Log Out</button>                
-								:
-								<button onClick={this.login}>Log In</button>              
-							}
+							<div className='title'>
+								<h1>Car Fleet DB Manager</h1>
+							</div>							
+							<div className="buttonHeader">
+								{this.state.user ? 
+									<span>
+										<div className='rightAlign'>Witaj, {this.state.user.displayName || this.state.user.email} </div>
+										<div onClick={this.logout}>Log Out</div>                
+									</span>
+									: <div onClick={this.login}>Log In</div>}
+							</div>
 					</div>
 				</header>
-				<div className='container'>
-					<section className='add-item'>
-						<form onSubmit={this.handleAddCar}>
-							<span>Add a car: </span>
-							<input type="text" name="id" placeholder="Car's ID" onChange={this.handleChange} value={this.state.id} />
-							<input type="text" name="registration_number" placeholder="Registration number" onChange={this.handleChange} value={this.state.registration_number} />
-							<input type="text" name="vin_number" placeholder="Vin number" onChange={this.handleChange} value={this.state.vin_number} />
-							<input type="text" name="brand" placeholder="Brand" onChange={this.handleChange} value={this.state.brand} />
-							<input type="text" name="model" placeholder="Model" onChange={this.handleChange} value={this.state.model} />
-							<input type="text" name="created_at" placeholder="Created at" onChange={this.handleChange} value={this.state.created_at} />
-							<input type="text" name="modified_at" placeholder="Modified at" onChange={this.handleChange} value={this.state.modified_at} />
-							<input type="text" name="mileage" placeholder="Mileage" onChange={this.handleChange} value={this.state.mileage} />
-							<input type="text" name="is_active" placeholder="Is active?" onChange={this.handleChange} value={this.state.is_active}/>
-							<button>Add</button>
-						</form>
-					</section>
-				</div>
-				<div className='container'>
-					<section className='display-cars'>
-						<ReactTable 
-						data={this.state.cars}
-						columns={[
-							{       
-								Header: "ID",
-								accessor: "id"
-							},
-							{
-								Header: "Registration No",
-								accessor: "registration_number"
-							},
-							{
-								Header: "Vin No",
-								accessor: "vin_number"
-							},
-							{
-								Header: "Brand",
-								accessor: "brand"
-							},
-							{
-								Header: "Model",
-								accessor: "model"
-							},
-							{
-								Header: "Created",
-								accessor: "created_at"
-							},
-							{
-								Header: "Modified",
-								accessor: "modified_at"
-							},
-							{
-								Header: "Mileage",
-								accessor: "mileage"
-							},
-							{
-								Header: "Is Active",
-								accessor: "is_active"
-							},
-							{
-								Header: "Remove",
-								accessor: "remove"
-							}
-						]}
-						defaultPageSize={20}
-						className="-striped -highlight"/>
-					</section>
-				</div>
+				
+				{this.state.user ?
+					<div>
+						<div className='container'>
+							<section className='add-item'>
+								<form onSubmit={this.handleAddCar}>
+									{/* <div><span>Add a car: </span></div> */}
+									<div><input type="text" name="id" placeholder="Car's ID" onChange={this.handleChange} value={this.state.id} /></div>
+									<div><input type="text" name="registration_number" placeholder="Registration number" onChange={this.handleChange} value={this.state.registration_number.toUpperCase()} /></div>
+									<div><input type="text" name="vin_number" placeholder="Vin number" onChange={this.handleChange} value={this.state.vin_number.toUpperCase()} /></div>
+									<div><input type="text" name="brand" placeholder="Brand" onChange={this.handleChange} value={this.state.brand.toUpperCase()} /></div>
+									<div><input type="text" name="model" placeholder="Model" onChange={this.handleChange} value={this.state.model.toUpperCase()} /></div>
+									<div><input type="text" name="created_at" placeholder="Created at" onChange={this.handleChange} value={this.state.created_at} /></div>
+									<div><input type="text" name="modified_at" placeholder="Modified at" onChange={this.handleChange} value={this.state.modified_at} /></div>
+									<div><input type="text" name="mileage" placeholder="Mileage" onChange={this.handleChange} value={this.state.mileage} /></div>
+									<div><input type="text" name="is_active" placeholder="Is active?" onChange={this.handleChange} value={this.state.is_active}/></div>
+									<div><button>Add new car</button></div>
+								</form>
+							</section>
+						</div>
+						<div className='container'>
+							<section className='display-cars'>
+								<ReactTable 
+								data={this.state.cars}
+								columns={[
+									{       
+										Header: "ID",
+										accessor: "id"
+									},
+									{
+										Header: "Registration No",
+										accessor: "registration_number"
+									},
+									{
+										Header: "Vin No",
+										accessor: "vin_number"
+									},
+									{
+										Header: "Brand",
+										accessor: "brand"
+									},
+									{
+										Header: "Model",
+										accessor: "model"
+									},
+									{
+										Header: "Created",
+										accessor: "created_at"
+									},
+									{
+										Header: "Modified",
+										accessor: "modified_at"
+									},
+									{
+										Header: "Mileage",
+										accessor: "mileage"
+									},
+									{
+										Header: "Is Active",
+										accessor: "is_active"
+									},
+									{
+										Header: "Remove",
+										accessor: "remove"
+									}
+								]}
+								defaultPageSize={20}
+								className="-striped -highlight"/>
+							</section>
+						</div>
+					</div>
+					:
+					<div className='container flex'>
+						<h2>Welcome on our Single Page Application. </h2>
+						<h2>Please log in to continue.</h2>
+					</div>
+				}
 			</div>
 		);
  	}
